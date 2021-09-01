0ccfef2a13cd94bf48a937a13fb0a7ecbb0687f2
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-09-01 21:14:48 +0100

    update

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import React from 'react';
 class App extends React.Component{
   state = {
   contacts: [],
-  name: ''
+    name: '',
+  number: '',
   }
 
-  handleNameChange=e=> {
- this.setState({name:e.currentTarget.value})
+  handleChange=e=> {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value,
+     
+ })
 }
 
   render() {
@@ -19,13 +23,27 @@ class App extends React.Component{
          <form>
            <label>
              Name
-<input
-           type="text"
-           value={this.state.name}
-            onChange={this.handleNameChange} 
+             <input
+              type="text"
+              name="name"
+               value={this.state.name}
+               onChange={this.handleChange} 
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
   title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
   required/>
+           </label>
+           <label>
+             Number
+             <input
+               type="tel"
+               name="number"
+               value={this.state.number}
+               onChange={this.handleChange}
+  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+  title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+  required
+             />
+             
            </label>
          </form>
          
