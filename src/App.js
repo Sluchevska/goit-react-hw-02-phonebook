a8ff7458a9eb26eb334425eb79980dec0a6ba342
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-09-01 21:39:46 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,56 +1,24 @@
-import logo from './logo.svg';
+import Form from './components/phonebook/Form';
 import './App.css';
 import React from 'react';
 
 class App extends React.Component{
-  state = {
-  contacts: [],
-    name: '',
-  number: '',
-  }
+  // state = {
+  // contacts: [],
+  //   name: '',
+  // number: '',
+  // }
 
-  handleChange=e=> {
-    this.setState({
-      [e.currentTarget.name]: e.currentTarget.value,
-     
- })
+  addContact = data => {
+    console.log(data)
   }
-  
-  handleSubmit = e => {
-    e.preventDefault()
-    console.log(this.state)
-  }
+ 
 
   render() {
      return (
        <div>
-         <form onSubmit={this.handleSubmit}> 
-           <label>
-             Name
-             <input
-              type="text"
-              name="name"
-               value={this.state.name}
-               onChange={this.handleChange} 
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-  title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-  required/>
-           </label>
-           <label>
-             Number
-             <input
-               type="tel"
-               name="number"
-               value={this.state.number}
-               onChange={this.handleChange}
-  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-  title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-  required
-             />
-             
-           </label>
-           <button type="submit">Add contact</button>
-         </form>
+         <Form onSubmit={this.addContact}
+         />
          
     </div>
   );
---
+++ src/components/phonebook/Form.js
@@ -0,0 +1,56 @@
+import { Component } from "react";
+
+class Form extends Component{
+    state = {
+        contacts: [],
+    name: '',
+  number: '',
+    }
+     handleChange=e=> {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value,
+     
+ })
+    }
+    
+  
+  handleSubmit = e => {
+      e.preventDefault()
+      this.props.onSubmit(this.state)
+    console.log(this.state)
+  }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}> 
+           <label>
+             Name
+             <input
+              type="text"
+              name="name"
+               value={this.state.name}
+               onChange={this.handleChange} 
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+  required/>
+           </label>
+           <label>
+             Number
+             <input
+               type="tel"
+               name="number"
+               value={this.state.number}
+               onChange={this.handleChange}
+  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+  title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+  required
+             />
+             
+           </label>
+           <button type="submit">Add contact</button>
+         </form>
+        );
+    }
+}
+
+export default Form
