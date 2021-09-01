458bc58789cd6cb22d304d45dd61f71f113aefea
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-09-01 20:38:07 +0100

    add input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import Input from './components/phonebook/Input';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
+class App extends React.Component{
+  state = {
+  contacts: [],
+  name: ''
+  }
+  render() {
+     return (
     <div>
-<input/>
+<Input/>
     </div>
   );
+  }
+
 }
+
 
 export default App;
--- src/components/phonebook/Input.js
+++ src/components/phonebook/Input.js
@@ -1,7 +1,7 @@
 export default function Input({type, name,title}) {
     return (
 <input
-            type={type}
+  type={type}
   name={name}
   pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
   title={title}
