e2c96e10f29a8b25e0285e8a0f04317071dc1e6e
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-09-01 20:34:40 +0100

    update

---
+++ src/components/phonebook/Input.js
@@ -0,0 +1,11 @@
+export default function Input({type, name,title}) {
+    return (
+<input
+            type={type}
+  name={name}
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title={title}
+  required
+/>
+    )
+}
