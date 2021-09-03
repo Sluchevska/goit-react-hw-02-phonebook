99af0be24147de9dabebc54213c153e2f37c9d6e
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-09-03 19:45:30 +0100

    add delete

--- src/App.js
+++ src/App.js
@@ -9,15 +9,17 @@ const { v4: uuidv4 } = require('uuid');
 
 class App extends React.Component{
   state = {
-    contacts: [ ],
+    contacts: [ {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
     
     filter: ''
   
   }
 
   
-
-   addContact = (name, number) => {
+      addContact = (name, number) => {
      const Contacts = {
       id: uuidv4(),
       name,
@@ -43,8 +45,8 @@ class App extends React.Component{
       contact.name.toLowerCase().includes(normalizedFilter))
  
   }
-    deleteContact = e => {
-        const contactId = e.currentTarget.value
+    deleteContact = contactId => {
+       
  
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact=>contact.id !== contactId)
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,36 +1,48 @@
 const { Component } = require('react');
 
- 
-class ContactList extends Component{
-    state = {
-  contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
-  filter: '',
-  name: '',
-  number: ''
-    }
+function ContactList({ contacts, onRemove }) {
+    return (
+         <ul>
+                {contacts.map(({ id, name, number}) => (
+                    <li key={id} name={name} number={number}>
+                    <span>{name} </span>
+                        <span>{number} </span>
+                        <button type="button" onClick={()=>onRemove(id)} > Delete contact</button>
+                    </li>
+               ))}
+            </ul>
+     )
+ }
+// class ContactList extends Component{
+//     state = {
+//   contacts: [
+//     {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+//     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+//     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+//     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+//   ],
+//   filter: '',
+//   name: '',
+//   number: ''
+//     }
 
 
   
       
-    render() {
+//     render() {
   
-        return (
-            <ul>
-                {this.state.contacts.map(({ id, name, number }) => (
-                    <li key={id} name={name} number={number}>
-                    <span>{name} </span>
-                        <span>{number} </span>
-                        <button type="button" > Delete contact</button>
-                    </li>
-               ))}
-            </ul>
-        )
-    }
-}
+//         return (
+//             <ul>
+//                 {this.state.contacts.map(({ id, name, number, onRemove }) => (
+//                     <li key={id} name={name} number={number}>
+//                     <span>{name} </span>
+//                         <span>{number} </span>
+//                         <button type="button" value={id} onClick={onRemove} > Delete contact</button>
+//                     </li>
+//                ))}
+//             </ul>
+//         )
+//     }
+// }
 
 export default ContactList
